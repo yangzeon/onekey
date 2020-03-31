424a9a0a69f491df7e033308200ca0580c4df948
yangzeon <yangzeon@gmail.com>
2020-03-31 17:36:04 +0800

    2
    2

--- c.sh
+++ c.sh
@@ -5,9 +5,9 @@
 #	Author: dylanbai8
 #	* 小内存VPS 一键安装 Caddy+PHP7+Sqlite3 环境 （支持VPS最小内存64M）
 #	* 一键绑定域名自动生成SSL证书开启https（ssl自动续期）、支持IPv6
-#	* 一键安装 typecho、wordpress、zblog、kodexplorer、laverna、一键整站备份
-#	* 一键安装 v2ray、rinetdbbr
-#	* 经典组合 [Website(caddy+php7+sqlite3+tls)+V2ray(vmess+websocket)]use_path+Rinetdbbr
+#	* 一键安装 typecho、、laverna、
+#	* 一键安装 v2ray、
+#	* 经典组合 [Website(caddy+php7+sqlite3+tls)+V2ray(vmess+websocket)]use_path
 #	* 推荐系统：Debian 8 （建议选择mini版）
 #	* 开源地址：https://github.com/dylanbai8/Onekey_Caddy_PHP7_Sqlite3
 #	Blog: https://oo0.bid
@@ -40,16 +40,6 @@ v2ray_conf_dir="${conf_dir}/v2ray"
 v2ray_conf="${v2ray_conf_dir}/config.json"
 }
 
-Default_rinetdbbr(){
-rinetdbbr_conf_dir="${conf_dir}/rinetdbbr"
-rinetdbbr_conf="${rinetdbbr_conf_dir}/config.conf"
-rinetdbbr_url="https://github.com/dylanbai8/Onekey_Caddy_PHP7_Sqlite3/raw/master/bbr"
-}
-
-win64_source(){
-bat_url="https://raw.githubusercontent.com/dylanbai8/Onekey_Caddy_PHP7_Sqlite3/master/zip"
-}
-
 #80端口用于签发验证ssl证书
 port1="80"
 #端口port2可自定义
@@ -71,16 +61,6 @@ wwwroot="/www"
 # https://github.com/typecho/typecho/releases
 typecho_path="https://github.com/typecho/typecho/releases/download/v1.1-17.10.30-release/1.1.17.10.30.-release.tar.gz"
 
-# https://github.com/kalcaddle/KodExplorer/releases
-kodcloud_path="https://github.com/kalcaddle/KodExplorer/archive/4.35.tar.gz"
-
-wordpress_path="https://wordpress.org/latest.tar.gz"
-# https://github.com/jumpstarter-io/wp-sqlite-integration
-wordpress_sqlite="https://downloads.wordpress.org/plugin/sqlite-integration.1.8.1.zip"
-
-# https://github.com/zblogcn/zblogphp/releases
-zblog_path="https://github.com/zblogcn/zblogphp/archive/1740.tar.gz"
-
 # https://github.com/Laverna/static-laverna
 laverna_path="https://github.com/Laverna/static-laverna/archive/gh-pages.zip"
 
@@ -98,18 +78,9 @@ if [[ -e ${conf_dir} ]]; then
 	echo -e "----------------------------------------------------------"
 	echo ""
 	echo -e "${Green} 一键安装 typecho 博客：${Font} bash c.sh -t"
-	echo -e "${Green} 一键安装 wordpress 博客：${Font} bash c.sh -w"
-	echo -e "${Green} 一键安装 zblog 博客：${Font} bash c.sh -z"
-	echo -e "${Green} 一键安装 kodexplorer 可道云：${Font} bash c.sh -k"
-	echo -e "${Green} 一键安装 laverna 印象笔记：${Font} bash c.sh -l"
-	echo -e "${Green} 一键整站备份：${Font} bash c.sh -a"
 	echo -e "${Green} 一键安装 v2ray 翻墙：${Font} bash c.sh -v"
-	echo -e "${Green} 一键安装 rinetd bbr 端口加速：${Font} bash c.sh -b"
 	echo ""
-	echo -e "${Green} 一键卸载 caddy：${Font} bash c.sh -unc"
-	echo -e "${Green} 一键卸载 php+sqlite：${Font} bash c.sh -unp"
 	echo -e "${Green} 一键卸载 v2ray：${Font} bash c.sh -unv"
-	echo -e "${Green} 一键卸载 rinetdbbr：${Font} bash c.sh -unb"
 	echo ""
 	echo -e "${Green} 提示：如果在上次执行中由于错误而中断了安装，请先执行 一键卸载 caddy ${Font}"
 	echo ""
@@ -251,53 +222,6 @@ EOF
 	fi
 }
 
-
-#卸载caddy
-uninstall_caddy(){
-Default_dir
-Default_caddy
-check_system
-
-echo -e "${OK} ${GreenBG} 正在卸载 caddy 请稍后 ... ${Font}"
-systemctl disable caddy >/dev/null 2>&1
-systemctl stop caddy >/dev/null 2>&1
-killall -9 caddy >/dev/null 2>&1
-
-rm -rf /usr/local/bin/caddy >/dev/null 2>&1
-rm -rf ${caddy_conf_dir} >/dev/null 2>&1
-rm -rf /etc/systemd/system/caddy.service >/dev/null 2>&1
-
-rm -rf ${wwwroot} >/dev/null 2>&1
-rm -rf /root/.caddy >/dev/null 2>&1
-rm -rf /root/.gnupg >/dev/null 2>&1
-rm -rf /root/.pki >/dev/null 2>&1
-rm -rf ${conf_dir} >/dev/null 2>&1
-echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-}
-
-
-#卸载php和sqlite
-uninstall_php_sqlite(){
-check_system
-
-if [[ "${ID}" == "centos" ]];then
-
-	echo -e "${OK} ${GreenBG} 正在卸载 php+sqlite 请稍后 ... ${Font}"
-	${INS} ${UNS} php70w-cgi php70w-fpm php70w-curl php70w-gd php70w-mbstring php70w-xml php70w-sqlite3 sqlite-devel -y >/dev/null 2>&1
-	${INS} ${UNS} unzip zip -y >/dev/null 2>&1
-	echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-
-else
-
-	echo -e "${OK} ${GreenBG} 正在卸载 php+sqlite 请稍后 ... ${Font}"
-	${INS} ${UNS} php7.0-cgi php7.0-fpm php7.0-curl php7.0-gd php7.0-mbstring php7.0-xml php7.0-sqlite3 sqlite3 -y >/dev/null 2>&1
-	${INS} ${UNS} unzip zip -y >/dev/null 2>&1
-	echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-
-fi
-}
-
-
 #卸载v2ray
 uninstall_v2ray(){
 Default_dir
@@ -315,74 +239,6 @@ rm -rf /etc/systemd/system/v2ray.service >/dev/null 2>&1
 
 ${INS} ${UNS} bc lsof ntpdate -y >/dev/null 2>&1
 echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-}
-
-
-#卸载bbr
-uninstall_bbr(){
-Default_dir
-Default_rinetdbbr
-check_system
-
-echo -e "${OK} ${GreenBG} 正在卸载 rinetdbbr 请稍后 ... ${Font}"
-systemctl disable rinetd-bbr >/dev/null 2>&1
-systemctl stop rinetd-bbr >/dev/null 2>&1
-killall -9 rinetd-bbr >/dev/null 2>&1
-
-rm -rf /usr/bin/rinetd-bbr >/dev/null 2>&1
-rm -rf ${rinetdbbr_conf_dir} >/dev/null 2>&1
-rm -rf /etc/systemd/system/rinetd-bbr.service >/dev/null 2>&1
-
-echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-}
-
-
-#卸载apache2
-uninstall_apache2(){
-
-if [[ "${ID}" == "centos" ]];then
-
-	systemctl disable httpd >/dev/null 2>&1
-	systemctl stop httpd >/dev/null 2>&1
-	killall -9 httpd >/dev/null 2>&1
-
-	rm -rf /etc/httpd >/dev/null 2>&1
-	rm -rf /etc/systemd/system/httpd.service >/dev/null 2>&1
-
-	${INS} ${UNS} httpd httpd-tools apr apr-util -y >/dev/null 2>&1
-
-	systemctl disable firewalld >/dev/null 2>&1
-	systemctl stop firewalld >/dev/null 2>&1
-	killall -9 firewalld >/dev/null 2>&1
-
-else
-
-	systemctl disable apache2 >/dev/null 2>&1
-	systemctl stop apache2 >/dev/null 2>&1
-	killall -9 apache2 >/dev/null 2>&1
-
-	rm -rf /etc/apache2 >/dev/null 2>&1
-	rm -rf /etc/systemd/system/apache2.service >/dev/null 2>&1
-
-	${INS} ${UNS} apache2 -y >/dev/null 2>&1
-
-fi
-}
-
-
-#强制清除可能残余的http服务 更新源
-apache_uninstall(){
-	echo -e "${OK} ${GreenBG} 正在强制清理可能残余的http服务 ${Font}"
-
-	uninstall_apache2
-
-	echo -e "${OK} ${GreenBG} 正在更新源 请稍后 ... ${Font}"
-
-	${add_source}
-	judge "系统更新"
-
-	${INS} install ntpdate bc lsof unzip zip -y
-	judge "必要软件 bc lsof unzip 安装"
 }
 
 
@@ -613,104 +469,6 @@ fi
 }
 
 
-#安装wordpress
-wordpress_install(){
-Default_dir
-if [[ -e ${conf_dir} ]]; then
-
-echo -e "${OK} ${GreenBG} 正在安装 wordpress 到 ${wwwroot} 目录 ${Font}"
-rm -rf ${wwwroot}
-mkdir ${wwwroot}
-
-wget --no-check-certificate ${wordpress_path} -O wordpress.tar.gz
-tar -zxvf wordpress.tar.gz -C ${wwwroot}
-mv ${wwwroot}/wordpress/* ${wwwroot}
-rm -rf ${wwwroot}/wordpress
-rm -rf wordpress.tar.gz
-
-wget --no-check-certificate ${wordpress_sqlite} -O sqlite.zip
-unzip sqlite.zip -d ${wwwroot}
-mv ${wwwroot}/wp-config-sample.php ${wwwroot}/wp-config.php
-mv ${wwwroot}/sqlite-integration ${wwwroot}/wp-content/plugins/
-mv ${wwwroot}/wp-content/plugins/sqlite-integration/db.php ${wwwroot}/wp-content/
-sed -i "s/define('DB_COLLATE', '');/define('DB_TYPE', 'sqlite');/g" ${wwwroot}/wp-config.php
-rm -rf sqlite.zip
-
-	chown www-data:www-data -R ${wwwroot}/*
-	chmod -R 777 ${wwwroot}
-
-echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-getdomain=$(cat ${conf_dir}/domain.txt)
-echo -e "${OK} ${GreenBG} 访问网站首页查看 http://${getdomain} ${Font}"
-
-else
-	echo -e "${Error} ${RedBG} 请先执行以下命令安装环境 ${Font}"
-	echo -e "${OK} ${GreenBG} wget -N --no-check-certificate https://raw.githubusercontent.com/yangzeon/onekey/master/c.sh && chmod +x c.sh && bash c.sh ${Font}"
-	exit 1
-fi
-}
-
-
-#安装zblog
-zblog_install(){
-Default_dir
-if [[ -e ${conf_dir} ]]; then
-
-echo -e "${OK} ${GreenBG} 正在安装 zblog 到 ${wwwroot} 目录 ${Font}"
-rm -rf ${wwwroot}
-mkdir ${wwwroot}
-
-wget --no-check-certificate ${zblog_path} -O zblog.tar.gz
-tar -zxvf zblog.tar.gz -C ${wwwroot}
-mv ${wwwroot}/*zblog*/* ${wwwroot}
-rm -rf ${wwwroot}/*zblog*
-rm -rf zblog.tar.gz
-
-	chown www-data:www-data -R ${wwwroot}/*
-	chmod -R 777 ${wwwroot}
-
-echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-getdomain=$(cat ${conf_dir}/domain.txt)
-echo -e "${OK} ${GreenBG} 访问网站首页查看 http://${getdomain} ${Font}"
-
-else
-	echo -e "${Error} ${RedBG} 请先执行以下命令安装环境 ${Font}"
-	echo -e "${OK} ${GreenBG} wget -N --no-check-certificate https://raw.githubusercontent.com/yangzeon/onekey/master/c.sh && chmod +x c.sh && bash c.sh ${Font}"
-	exit 1
-fi
-}
-
-
-#安装kedexplorer
-kodexplorer_install(){
-Default_dir
-if [[ -e ${conf_dir} ]]; then
-
-echo -e "${OK} ${GreenBG} 正在安装 kedexplorer 到 ${wwwroot} 目录 ${Font}"
-rm -rf ${wwwroot}
-mkdir ${wwwroot}
-
-wget --no-check-certificate ${kodcloud_path} -O kodcloud.tar.gz
-tar -zxvf kodcloud.tar.gz -C ${wwwroot}
-mv ${wwwroot}/*KodExplorer*/* ${wwwroot}
-rm -rf ${wwwroot}/*KodExplorer*
-rm -rf kodcloud.tar.gz
-
-	chown www-data:www-data -R ${wwwroot}/*
-	chmod -R 777 ${wwwroot}
-
-echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-getdomain=$(cat ${conf_dir}/domain.txt)
-echo -e "${OK} ${GreenBG} 访问网站首页查看 http://${getdomain} ${Font}"
-
-else
-	echo -e "${Error} ${RedBG} 请先执行以下命令安装环境 ${Font}"
-	echo -e "${OK} ${GreenBG} wget -N --no-check-certificate https://raw.githubusercontent.com/yangzeon/onekey/master/c.sh && chmod +x c.sh && bash c.sh ${Font}"
-	exit 1
-fi
-}
-
-
 #安装laverna
 laverna_install(){
 Default_dir
@@ -733,30 +491,6 @@ rm -rf laverna.zip
 echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
 getdomain=$(cat ${conf_dir}/domain.txt)
 echo -e "${OK} ${GreenBG} 访问网站首页查看 http://${getdomain} ${Font}"
-
-else
-	echo -e "${Error} ${RedBG} 请先执行以下命令安装环境 ${Font}"
-	echo -e "${OK} ${GreenBG} wget -N --no-check-certificate https://raw.githubusercontent.com/yangzeon/onekey/master/c.sh && chmod +x c.sh && bash c.sh ${Font}"
-	exit 1
-fi
-}
-
-
-#整站备份
-bak_wwwroot(){
-Default_dir
-if [[ -e ${conf_dir} ]]; then
-
-echo -e "${OK} ${GreenBG} 正在整站备份（含数据库） ${Font}"
-
-unzip_password_w=`cat /dev/urandom | head -n 10 | md5sum | head -c 8`
-rm -rf ${wwwroot}/www.zip
-zip -q -r -P ${unzip_password_w} ${wwwroot}/www.zip ${wwwroot}
-
-getdomain=$(cat ${conf_dir}/domain.txt)
-echo -e "${OK} ${GreenBG} 操作已完成 ${Font}"
-echo -e "${OK} ${GreenBG} 下载地址为：http:\\${getdomain}\www.zip ${Font}"
-echo -e "${OK} ${Green} 解压密码（由函数随机生成）：${Font} ${unzip_password_w}"
 
 else
 	echo -e "${Error} ${RedBG} 请先执行以下命令安装环境 ${Font}"
@@ -864,37 +598,6 @@ judge "客户端json配置"
 }
 
 
-#生成Windows客户端
-win64_v2ray(){
-	win64_source
-	TAG_URL="https://api.github.com/repos/v2ray/v2ray-core/releases/latest"
-	NEW_VER=`curl -s ${TAG_URL} --connect-timeout 10| grep 'tag_name' | cut -d\" -f4`
-
-	echo -e "${OK} ${GreenBG} 正在生成Windows客户端 v2ray-core最新版本 ${NEW_VER} ${Font}"
-	rm -rf ./V2rayPro
-	mkdir ./V2rayPro
-	mkdir ./V2rayPro/v2ray
-	wget --no-check-certificate https://github.com/v2ray/v2ray-core/releases/download/${NEW_VER}/v2ray-windows-64.zip -O v2ray.zip
-	unzip v2ray.zip -d ./V2rayPro/v2ray
-	wget --no-check-certificate ${bat_url} -O bat.zip
-	unzip bat.zip
-	mv bat ./V2rayPro/start.bat
-	mv exe ./V2rayPro/v2ray/wv2ray-service.exe
-
-	v2ray_user_config
-
-	echo -e "${OK} ${GreenBG} 正在打包 v2ray-windows-64 客户端 ${Font}"
-
-	rm -rf ${wwwroot}/V2rayPro.zip
-	zip -q -r -P ${unzip_password_v} ${wwwroot}/V2rayPro.zip ./V2rayPro
-	judge "Windows 客户端打包成功"
-
-	rm -rf v2ray.zip
-	rm -rf bat.zip
-	rm -rf ./V2rayPro
-}
-
-
 #展示v2ray客户端配置信息
 v2ray_information(){
 	clear
@@ -914,10 +617,6 @@ v2ray_information(){
 	echo -e "${Green} 伪装类型（ws host）：${Font} 留空"
 	echo -e "${Green} 伪装路径（ws path）：${Font} /${getv2ray_path}"
 	echo -e "${Green} 底层传输安全：${Font} tls"
-	echo ""
-	echo -e "${Green} 注意：伪装路径不要少写 [ / ] ${Font}"
-	echo -e "${Green} Windows系统64位客户端下载：${Font} http:\\${getdomain}\V2rayPro.zip"
-	echo -e "${Green} 解压密码（由函数随机生成）：${Font} ${unzip_password_v}"
 	echo ""
 	echo -e "----------------------------------------------------------"
 }
@@ -985,63 +684,6 @@ else
 fi
 }
 
-
-#安装bbr端口加速
-rinetdbbr_install(){
-Default_dir
-if [[ -e ${conf_dir} ]]; then
-echo -e "${OK} ${GreenBG} 正在安装 Rinetdbbr 请稍后 ... ${Font}"
-	Default_rinetdbbr
-	rm -rf ${rinetdbbr_conf_dir}
-	mkdir ${rinetdbbr_conf_dir}
-
-	getport2=$(cat ${conf_dir}/port2.txt)
-
-	wget --no-check-certificate ${rinetdbbr_url} -O rinetdbbr
-	mv rinetdbbr /usr/bin/rinetd-bbr
-	chmod +x /usr/bin/rinetd-bbr
-	judge "rinetd-bbr 安装"
-
-	IFACE=$(ip -4 addr | awk '{if ($1 ~ /inet/ && $NF ~ /^[ve]/) {a=$NF}} END{print a}')
-
-	touch ${rinetdbbr_conf}
-	cat <<EOF >> ${rinetdbbr_conf}
-0.0.0.0 ${getport2} 0.0.0.0 ${getport2}
-EOF
-
-	touch /etc/systemd/system/rinetd-bbr.service
-	cat <<EOF > /etc/systemd/system/rinetd-bbr.service
-[Unit]
-Description=Rinetdbbr Service
-After=network.target
-Wants=network.target
-
-[Service]
-Type=simple
-PIDFile=/var/run/rinetd-bbr.pid
-ExecStart=/usr/bin/rinetd-bbr -f -c ${rinetdbbr_conf_dir} raw ${IFACE}
-RestartPreventExitStatus=23
-Restart=always
-User=root
-
-[Install]
-WantedBy=multi-user.target
-EOF
-	judge "rinetd-bbr 自启动配置"
-
-	systemctl daemon-reload
-	systemctl enable rinetd-bbr >/dev/null 2>&1
-	systemctl start rinetd-bbr
-	judge "加速端口：${getport2} 启动 rinetd-bbr"
-
-else
-	echo -e "${Error} ${RedBG} 请先执行以下命令安装环境 ${Font}"
-	echo -e "${OK} ${GreenBG} wget -N --no-check-certificate https://raw.githubusercontent.com/yangzeon/onekey/master/c.sh && chmod +x c.sh && bash c.sh ${Font}"
-	exit 1
-fi
-}
-
-
 #命令块执行列表
 main(){
 	is_root
@@ -1070,38 +712,14 @@ if [[ $# > 0 ]];then
 		-t|--typecho_install)
 		typecho_install
 		;;
-		-k|--kodexplorer_install)
-		kodexplorer_install
-		;;
-		-w|--wordpress_install)
-		wordpress_install
-		;;
-		-z|--zblog_install)
-		zblog_install
-		;;
 		-l|--laverna_install)
 		laverna_install
-		;;
-		-a|--bak_wwwroot)
-		bak_wwwroot
 		;;
 		-v|--v2ray_install)
 		v2ray_install
 		;;
-		-b|--rinetdbbr_install)
-		rinetdbbr_install
-		;;
-		-unc|--uninstall_caddy)
-		uninstall_caddy
-		;;
-		-unp|--uninstall_php_sqlite)
-		uninstall_php_sqlite
-		;;
 		-unv|--uninstall_v2ray)
 		uninstall_v2ray
-		;;
-		-unb|--uninstall_bbr)
-		uninstall_bbr
 		;;
 	esac
 else
