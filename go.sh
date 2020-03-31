250abc5c9d76f8074e0565e266b1bd82f0b45b07
yangzeon <yangzeon@gmail.com>
2020-03-31 17:58:05 +0800

    2
    2

--- go.sh
+++ go.sh
@@ -10,16 +10,21 @@
 #====================================================
 
 #START
+
 echo "================================开始================================ "
+
+
 mkdir /root/.ssh
 cd /root/.ssh
 yum install wget nano -y
 echo "================================正在加载 KEY 请稍后 ================================"
+
 wget https://raw.githubusercontent.com/yangzeon/onekey/master/10GKVM.pub
 cat 10GKVM.pub >> authorized_keys
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 echo "================================正在更改 ssh 请稍后 ================================ "
+
 cd
 
 sed -i 's/#RSAAuthentication yes/RSAAuthentication yes/' /etc/ssh/sshd_config
@@ -29,4 +34,11 @@ sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_c
 echo "================================ 完成 ... ================================"
 /etc/init.d/sshd restart
 service sshd restart
+
+echo "================================ 定时重启 ... ================================"
+
+echo "0 4 * * * root reboot >> /tmp/reboot.txt" >> /etc/crontab
+echo "30 4 10 * * root yum update && apt-get dist-upgrade -y >> /tmp/update.txt" >> /etc/crontab
+
+echo "================================ 继续 ... ================================"
 wget -N --no-check-certificate https://raw.githubusercontent.com/yangzeon/onekey/master/c.sh && chmod +x c.sh && bash c.sh
